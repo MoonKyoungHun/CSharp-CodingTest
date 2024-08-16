eb32d642a23c050a3d7b2ba3c39e3b9bf9656ab3
MoonKyoungHun <lsf8355@gmail.com>
2024-08-16 11:43:30 +0900

    문자열 출력하기
    문자열 출력하기

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+        internal class Program
+        {
+            // 문제 설명
+            // 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+            // 입력 #1
+            // HelloWorld!
+            // 출력 #1
+            // HelloWorld!
+
+            public class Example
+            {
+                public static void Main()
+                {
+                    String s;
+
+                    Console.Clear();
+                    s = Console.ReadLine();
+                    Console.WriteLine(s);
+                }
+            }
+        }
+}
